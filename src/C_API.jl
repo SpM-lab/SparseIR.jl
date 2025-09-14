7fbc29a50947ba2ec7775d23fc8dac0b6b4bee0a
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2025-09-14 16:09:35 +0900

    Add debug mode

--- src/C_API.jl
+++ src/C_API.jl
@@ -3,7 +3,23 @@ module C_API
 using CEnum: CEnum, @cenum
 using libsparseir_jll
 
-const libsparseir = libsparseir_jll.libsparseir
+function get_libsparseir()
+    # Use debug library if SPARSEIR_LIB_PATH environment variable is set
+    if haskey(ENV, "SPARSEIR_LIB_PATH")
+        debug_path = ENV["SPARSEIR_LIB_PATH"]
+        if isfile(debug_path)
+            @info "Using debug library: $debug_path"
+            return debug_path
+        else
+            @warn "Debug library not found at $debug_path, falling back to JLL"
+        end
+    end
+    
+    # Production: use JLL package
+    return libsparseir_jll.libsparseir
+end
+
+const libsparseir = get_libsparseir()
 const c_complex = ComplexF64
 
 mutable struct _spir_kernel end
