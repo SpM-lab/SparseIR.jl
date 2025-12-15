930827f0f2a87303a6a94ea08484cc4aea699f9f
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2025-12-15 20:12:52 +0900

    Del unused sh

--- run_with_locally_build_lib.sh
+++
@@ -1,7 +0,0 @@
-rm -rf ~/.julia/compiled/v*/SparseIR
-
-export SPARSEIR_LIB_PATH="/Users/hiroshi/projects/sparse-ir/sparseir-rust/target/release/libsparse_ir_capi.dylib"
-export RUST_BACKTRACE=1
-export SPARSEIR_DEBUG=1
-
-julia --project=@. -e "using Pkg; Pkg.test()"
