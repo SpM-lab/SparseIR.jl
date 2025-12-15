1aded3a672620226a67c12209f76a0c2a729d960
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2025-12-15 20:22:03 +0900

    Fix

--- src/C_API.jl
+++ src/C_API.jl
@@ -9,6 +9,7 @@ function get_libsparseir()
     deps_dir = joinpath(dirname(@__DIR__), "deps")
     local_libsparseir_path = joinpath(deps_dir, "libsparse_ir_capi.$(Libdl.dlext)")
     if isfile(local_libsparseir_path)
+        @info "Using local libsparseir: $local_libsparseir_path"
         return local_libsparseir_path
     else
         return libsparseir_jll.libsparseir
